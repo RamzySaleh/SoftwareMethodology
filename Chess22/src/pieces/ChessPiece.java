fa22b5622ca28332ff0e456a6213cf7e17001f1a
Ramzy Saleh <rs1064@scarletmail.rutgers.edu>
2016-03-15 02:46:24 -0400

    Set color and position methods + Helper method

--- Chess22/src/pieces/ChessPiece.java
+++ Chess22/src/pieces/ChessPiece.java
@@ -13,4 +13,26 @@ public class ChessPiece {
 	public boolean isPathValid(String destination){
 		return false;
 	}
+	public void setCurrentPosition(String position){
+		currentPosition = position;
+	}
+	public void setColor(String color){
+		this.color = color;
+	}
+	public int[] positionStringToArr(String position){		
+		int[] orderedPair = new int[2];		
+		orderedPair[0] = -1;		
+		orderedPair[1] = -1;		
+		 			
+		position = position.toLowerCase();		
+		 			
+		if (position.length() != 2) return orderedPair;		
+		 			
+		int letterToInt = position.charAt(0)-'a'+1;		
+		orderedPair[0] = letterToInt;		
+		orderedPair[1] = (int)position.charAt(1);		
+		 			
+		return orderedPair;				
+	}
+
 }
