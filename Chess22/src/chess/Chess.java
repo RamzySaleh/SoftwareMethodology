f5e5beb26b91440190b29179723f69c3afcba9ae
Ramzy Saleh <rs1064@scarletmail.rutgers.edu>
2016-03-21 07:40:41 -0400

    Now, you must get your King out of check and you cannot put your own king in check

--- Chess22/src/chess/Chess.java
+++ Chess22/src/chess/Chess.java
@@ -10,205 +10,25 @@ import pieces.*;
 
 public class Chess {
 
-	/**
-	 * TODO Finish implementation
-	 * 
-	 * @param args
-	 */
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-		ChessBoard chessBoard = new ChessBoard();
-		//chessBoard.printBoard();
 		
+		ChessBoard chessBoard = new ChessBoard();
+
 		Boolean drawRequested = false;
 		Boolean drawAccepted = false;
 		Boolean legalMove = false;
 		String turn = "White's move:";
 		String winner = "";
 		Boolean resign = false;
+		Boolean isCheckDetected = false;
 		Boolean checkMate = false;
 		Boolean staleMate = false;
-		
-		System.out.println("");
-		
 		Scanner scanner = new Scanner (System.in);
 		
+		System.out.println(intToChar(0));
 		while (!drawAccepted && !resign && !checkMate && !staleMate) {
-			
-			chessBoard.printBoard();
-			System.out.println("");
-			System.out.print(turn);
-			String requestedMove = scanner.nextLine();
-			
-			
-			// Check if the user is requesting a draw.
-			// If there was no draw requested, then saying 'draw' is an illegal move.
-			// If there was a draw requested, then the game ends in a draw.
-			// If there is a resign, the game ends. 
-			if (drawRequested && requestedMove.equals("draw")){
-				drawAccepted = true; 
-				break;
-			} else if (!drawRequested && requestedMove.equals("draw")){
-				System.out.println("Illegal move, try again");
-				continue;
-			} else if (requestedMove.equals("resign")){
-				resign = true;
-				if(turn.equals("White's move:")){
-					winner = "Black wins";
-				} else {
-					winner = "White wins";
-				}
-				break;
-			}
-			
-			// Split the move based on spaces.
-			String[] requestedMoveArr = requestedMove.split("\\s+");
-			
-			// We should now check if the array is of size the right size.
-			// It should be of length 2 or 3 - ex: "a5 e3 N" (promotion) or "a5 e3". We checked the cases with one input,
-			// which is 'draw' or 'resign'. 
-			if(requestedMoveArr.length != 2){
-				if(requestedMoveArr.length != 3){
-				System.out.println("Error 1 Illegal move, try again");
-				continue;
-				}
-			}
-			
-			// Find the piece the user wants to move.
-			ChessPiece movingPiece = chessBoard.findPieceAtLocation(requestedMoveArr[0]);
-			String destination = requestedMoveArr[1];
-			
-			// Either the user is trying to move a piece that is not in the
-			// requested spot, or the entered piece location is not of the right 
-			// form. (Examples include "ee1ee" or "t3" or "113232", etc.)
-			if (movingPiece == null) {
-				System.out.println("Error 2 Illegal move, try again");
-				continue;
-			}
-			
-			
-			// Check if the user is moving their piece
-			if(turn.equals("White's move:")){
-				if (movingPiece.color.equals("black")) {
-					System.out.println("Error 6 Illegal move, try again");
-					continue;
-				}
-			} else {
-				if (movingPiece.color.equals("white")) {
-					System.out.println("Error 7 Illegal move, try again");
-					continue;
-				}
-			}
-			
-			// Checks if the piece can travel along this path and
-			// whether the path is clear.
-			if (!movingPiece.isPathValid(destination) || !chessBoard.isPathClear(movingPiece, destination)){
-				
-				// If it isn't a Pawn or a King, we know right away that it is illegal.
-				// We will now check for enpassant in the else statement.
-				if(!(movingPiece instanceof Pawn)){
-					System.out.println("Error 3 Illegal move, try again");
-					continue;
-				} else {
-					
-					// Cannot perform enpassant, if there is nothing in the destination.
-					if(chessBoard.findPieceAtLocation(destination) == null){
-						int test;
-						if(movingPiece.color.equals("white")){
-							test = (Character.getNumericValue(destination.charAt(1)))-1;
-						}
-						else{
-							test = (Character.getNumericValue(destination.charAt(1)))+1;
-						}
-						
-						String newDest = destination.charAt(0)+(Integer.toString(test));
 
-						if((chessBoard.findPieceAtLocation(newDest) != null) && ((chessBoard.findPieceAtLocation(newDest) instanceof Pawn))){
-							Pawn tempPawn = (Pawn) (chessBoard.findPieceAtLocation(newDest));
-							if(tempPawn.justMovedTwo == true){
-								chessBoard.enPassant(requestedMoveArr[0], destination, newDest);
-								continue;
-							}
-						}
-						System.out.println("Error 4 Illegal move, try again");
-						continue;
-					}
-					
-					ChessPiece destPiece = chessBoard.findPieceAtLocation(destination);
-					
-					int[] movingOrderedPair = movingPiece.positionStringToArr(movingPiece.currentPosition);
-					int[] destinOrderedPair = destPiece.positionStringToArr(destination);
-					
-					// If the Pawn is not a direct diagonal, then it is an illegal move.
-					if(Math.abs(movingOrderedPair[0]-destinOrderedPair[0])!=1 ||
-							Math.abs(movingOrderedPair[1]-destinOrderedPair[1])!=1){
-						System.out.println("Illegal move 600, try again");
-						continue;
-					}
-					
-				}
-			}
-			//Ensure that player cannot capture member of their own team
-			if(turn.equals("White's move:")){
-				if((chessBoard.findPieceAtLocation(destination) != null) &&(chessBoard.findPieceAtLocation(destination)).color.equals("white")){
-					System.out.println("Error 20 Illegal move, try again");
-					continue;
-				}
-			}
-			else{
-				if(((chessBoard.findPieceAtLocation(destination) != null) && (chessBoard.findPieceAtLocation(destination)).color.equals("black"))){
-					System.out.println("Error 21 Illegal move, try again");
-					continue;
-				}
-				
-			}
-			/**
-			 * IMPLICIT PROMOTION (pawn is promoted to queen without explicit choice)
-			 */
-			if((movingPiece instanceof Pawn) && (destination.charAt(1) == '8' || destination.charAt(1) == '1') && (requestedMoveArr.length != 3)){
-				chessBoard.movePiece(requestedMoveArr[0], requestedMoveArr[1], 'Q');
-				continue;
-			}
-			
-			/**
-			 * EXPLICIT PROMOTION (player chooses desired promotion)
-			 */
-			if(requestedMoveArr.length == 3){
-				if(requestedMoveArr[2].equals("draw?")){
-					drawRequested = true;
-				}
-				//Checking to see if the pawn actually reached the end of the board
-				else if((movingPiece instanceof Pawn) && (destination.charAt(1) == '8' || destination.charAt(1) == '1')){
-				switch(requestedMoveArr[2]){
-				case "Q":
-					chessBoard.movePiece(requestedMoveArr[0], requestedMoveArr[1], 'Q');
-					continue;
-				case "N":
-					chessBoard.movePiece(requestedMoveArr[0], requestedMoveArr[1], 'N');
-					continue;
-				case "K":
-					chessBoard.movePiece(requestedMoveArr[0], requestedMoveArr[1], 'K');
-					continue;
-				case "B":
-					chessBoard.movePiece(requestedMoveArr[0], requestedMoveArr[1], 'B');
-					continue;
-				case "R":
-					chessBoard.movePiece(requestedMoveArr[0], requestedMoveArr[1], 'R');
-					continue;
-				default:
-					System.out.println("Illegal move, try again");
-					continue;
-				}
-				}
-				else {
-					System.out.println("Illegal move, try again");
-					continue;
-				}
-			}
-			
-			legalMove = true;
-			
 			/** 
 			 *  Split user input by space. 
 			 * 	Task 1: Check if they are moving their piece
@@ -227,14 +47,210 @@ public class Chess {
 			 *         		
 			*/
 			
-			if (legalMove) {
-				System.out.println("In here!!!!");
-				chessBoard.movePiece(requestedMoveArr[0], requestedMoveArr[1], 'x');
+			
+			System.out.println("");
+			chessBoard.printBoard();
+			System.out.println("");
+			if(isCheckDetected) System.out.println("Check");
+			System.out.print(turn);
+			String requestedMove = scanner.nextLine();
+			
+			
+			/** Check if the user is requesting a draw.
+			* If there was no draw requested, then saying 'draw' is an illegal move.
+			* If there was a draw requested, then the game ends in a draw.
+			* If there is a resign, the game ends.
+			*/ 
+			if (drawRequested && requestedMove.equals("draw")){
+				drawAccepted = true; 
+				break;
+			} else if (!drawRequested && requestedMove.equals("draw")){
+				System.out.println("Illegal move, try again");
+				legalMove = false;
+				continue;
+			} else if (requestedMove.equals("resign")){
+				resign = true;
 				if(turn.equals("White's move:")){
-					chessBoard.isCheckDetected("white");
+					winner = "Black wins";
+				} else {
+					winner = "White wins";
+				}
+				break;
+			}
+			
+			// Split the move based on spaces.
+			String[] requestedMoveArr = requestedMove.split("\\s+");
+			
+			/** We should now check if the array is of size the right size.
+			* It should be of length 2 or 3 - ex: "a5 e3 N" (promotion) or "a5 e3". We checked the cases with one input,
+			* which is 'draw' or 'resign'. 
+			*/
+			if(requestedMoveArr.length != 2){
+				if(requestedMoveArr.length != 3){
+					System.out.println("Error 1 Illegal move, try again");
+					legalMove = false;
+					continue;
+				}
+			}
+			
+			// Find the piece the user wants to move.
+			ChessPiece movingPiece = chessBoard.findPieceAtLocation(requestedMoveArr[0]);
+			String destination = requestedMoveArr[1];
+			
+			/** Either the user is trying to move a piece that is not in the
+			* requested spot, or the entered piece location is not of the right 
+			* form. (Examples include "ee1ee" or "t3" or "113232", etc.)
+			*/
+			if (movingPiece == null) {
+				System.out.println("Error 2 Illegal move, try again");
+				legalMove = false;
+				continue;
+			}
+			
+			
+			// Check if the user is moving their piece
+			if(turn.equals("White's move:")){
+				if (movingPiece.color.equals("black")) {
+					System.out.println("Error 6 Illegal move, try again");
+					legalMove = false;
+					continue;
+				}
+			} else {
+				if (movingPiece.color.equals("white")) {
+					System.out.println("Error 7 Illegal move, try again");
+					legalMove = false;
+					continue;
+				}
+			}
+			
+			// Checks if the piece can travel along this path and whether the path is clear.
+			if (!movingPiece.isPathValid(destination) || !chessBoard.isPathClear(movingPiece, destination)){
+				
+				// If it isn't a Pawn or a King, we know right away that it is illegal.
+				if(!(movingPiece instanceof Pawn)){
+					System.out.println("Error 3 Illegal move, try again");
+					legalMove = false;
+					continue;
+				} else {
+					
+					if(chessBoard.findPieceAtLocation(destination) == null){
+						
+						int test;
+						if(movingPiece.color.equals("white")){
+							test = (Character.getNumericValue(destination.charAt(1)))-1;
+						}
+						else{
+							test = (Character.getNumericValue(destination.charAt(1)))+1;
+						}
+						
+						String newDest = destination.charAt(0)+(Integer.toString(test));
+
+						if((chessBoard.findPieceAtLocation(newDest) != null) && ((chessBoard.findPieceAtLocation(newDest) instanceof Pawn))){
+							Pawn tempPawn = (Pawn) (chessBoard.findPieceAtLocation(newDest));
+							if(tempPawn.justMovedTwo == true){
+								chessBoard.enPassant(requestedMoveArr[0], destination, newDest);
+								continue;
+							}
+						}
+						
+						System.out.println("Error 4 Illegal move, try again");
+						legalMove = false;
+						continue;
+					}
+					
+					ChessPiece destPiece = chessBoard.findPieceAtLocation(destination);
+					
+					int[] movingOrderedPair = movingPiece.positionStringToArr(movingPiece.currentPosition);
+					int[] destinOrderedPair = destPiece.positionStringToArr(destination);
+					
+					// If the Pawn is not a direct diagonal, then it is an illegal move.
+					if(Math.abs(movingOrderedPair[0]-destinOrderedPair[0])!=1 ||
+							Math.abs(movingOrderedPair[1]-destinOrderedPair[1])!=1){
+						System.out.println("Illegal move 600, try again");
+						legalMove = false;
+						continue;
+					}
+					
+				}
+			}
+			// Ensure that player cannot capture member of their own team
+			if(turn.equals("White's move:")){
+				if((chessBoard.findPieceAtLocation(destination) != null) && (chessBoard.findPieceAtLocation(destination)).color.equals("white")){
+					System.out.println("Error 20 Illegal move, try again");
+					legalMove = false;
+					continue;
+				}
+			} else{
+				if(((chessBoard.findPieceAtLocation(destination) != null) && (chessBoard.findPieceAtLocation(destination)).color.equals("black"))){
+					System.out.println("Error 21 Illegal move, try again");
+					legalMove = false;
+					continue;
+				}	
+			}
+			/**
+			 * IMPLICIT PROMOTION (pawn is promoted to queen without explicit choice)
+			 */
+			if((movingPiece instanceof Pawn) && (destination.charAt(1) == '8' || destination.charAt(1) == '1') && (requestedMoveArr.length != 3)){
+				if(!chessBoard.movePiece(requestedMoveArr[0], requestedMoveArr[1], 'Q')) System.out.println("Moved without regard to check, Illegal move, try again");
+				legalMove = false;
+				continue;
+			}
+			
+			/**
+			 * EXPLICIT PROMOTION (player chooses desired promotion)
+			 */
+			if(requestedMoveArr.length == 3){
+				if(requestedMoveArr[2].equals("draw?")){
+					drawRequested = true;
+				} // Checking to see if the pawn actually reached the end of the board 
+				else if((movingPiece instanceof Pawn) && (destination.charAt(1) == '8' || destination.charAt(1) == '1')){
+					switch(requestedMoveArr[2]){
+					case "Q":
+						boolean returnVal = chessBoard.movePiece(requestedMoveArr[0], requestedMoveArr[1], 'Q');
+						if(!returnVal) System.out.println("Moved without regard to check, Illegal move, try again");
+						continue;
+					case "N":
+						if(!chessBoard.movePiece(requestedMoveArr[0], requestedMoveArr[1], 'N')) System.out.println("Moved without regard to check, Illegal move, try again");
+						continue;
+					case "K":
+						if(!chessBoard.movePiece(requestedMoveArr[0], requestedMoveArr[1], 'K')) System.out.println("Moved without regard to check, Illegal move, try again");
+						continue;
+					case "B":
+						if(!chessBoard.movePiece(requestedMoveArr[0], requestedMoveArr[1], 'B')) System.out.println("Moved without regard to check, Illegal move, try again");
+						continue;
+					case "R":
+						if(!chessBoard.movePiece(requestedMoveArr[0], requestedMoveArr[1], 'R')) System.out.println("Moved without regard to check, Illegal move, try again");
+						continue;
+					default:
+						System.out.println("Illegal move, try again");
+						continue;
+					}
+				}
+				else {
+					System.out.println("Illegal move, try again");
+					continue;
+				}
+			}
+			legalMove = true;
+			if (legalMove) {
+				boolean returnVal = chessBoard.movePiece(requestedMoveArr[0], requestedMoveArr[1], 'x');
+				if(!returnVal){ 
+					System.out.println("Moved without regard to check, Illegal move, try again");
+					continue;
+				}
+				if(turn.equals("White's move:")){
+					if(chessBoard.isCheckDetected("white")){
+						isCheckDetected = true;
+					} else {
+						isCheckDetected = false;
+					}
 					turn = "Black's move:";
 				} else {
-					chessBoard.isCheckDetected("black");
+					if(chessBoard.isCheckDetected("black")){
+						isCheckDetected = true;
+					} else {
+						isCheckDetected = false;
+					}
 					turn = "White's move:";
 				}
 			}
@@ -248,7 +264,6 @@ public class Chess {
 			System.out.println(winner);
 		}
 		scanner.close();
-		
 		
 	}
 	
