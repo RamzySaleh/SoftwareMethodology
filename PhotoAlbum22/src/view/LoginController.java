125643bed7e6b70c6c06fbd4ce4f37bad8c84695
Sara Zayed <smmzayed@gmail.com>
2016-03-29 09:13:58 -0400

    Display admin window when logging in with "admin"

--- PhotoAlbum22/src/view/LoginController.java
+++ PhotoAlbum22/src/view/LoginController.java
@@ -1,5 +1,6 @@
 package view;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Optional;
@@ -7,23 +8,27 @@ import app.User;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.Dialog;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.control.SplitPane;
 import javafx.scene.control.TextField;
 import javafx.scene.control.TitledPane;
 import javafx.scene.input.MouseEvent;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.stage.Stage;
+import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.control.Alert.AlertType;
 
-public class LoginController {
+public class LoginController{
 	@FXML AnchorPane window;
 	@FXML TextField username;
 	@FXML Button enter;
@@ -32,7 +37,7 @@ public class LoginController {
 	public void enterButtonClicked(ActionEvent e){
 
 		String user = username.getText();
-		if(user.equals("") || user == null || username.isEditable()){
+		if(user.equals("") || user == null){
 			Alert alert = new Alert(AlertType.ERROR);
 			alert.setHeaderText("Error");
 			alert.setContentText("Must input a username");
@@ -40,7 +45,23 @@ public class LoginController {
 			return;
 		}
 		
-		if(searchForUser(users, user) == -1){
+		if(user.equalsIgnoreCase("admin")){
+			try {
+				Stage stage = new Stage();
+				FXMLLoader loader = new FXMLLoader();
+				loader.setLocation(getClass().getResource("admin.fxml"));
+				SplitPane rootLayout = (SplitPane) loader.load();
+				Scene scene = new Scene(rootLayout);
+				scene.getStylesheets().add("/view/application.css");
+				stage.setScene(scene);
+				((Node)e.getSource()).getScene().getWindow().hide();
+				stage.show();	
+				
+			} catch (IOException m) {
+				m.printStackTrace();
+			}
+		}
+		else if(searchForUser(users, user) == -1){
 			Alert alert = new Alert(AlertType.ERROR);
 			alert.setHeaderText("Error");
 			alert.setContentText("User does not exist");
