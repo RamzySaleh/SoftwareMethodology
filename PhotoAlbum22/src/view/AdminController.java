2f5ced9af2d2876828e2c0679eec1364e7f1b47c
Sara Zayed <smmzayed@gmail.com>
2016-03-31 10:23:25 -0400

    Added main page controller for user

--- PhotoAlbum22/src/view/AdminController.java
+++ PhotoAlbum22/src/view/AdminController.java
@@ -3,7 +3,6 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Optional;
-
 import app.User;
 import view.MainController;
 import javafx.event.ActionEvent;
