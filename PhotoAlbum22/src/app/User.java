598eac7e336981e7b050f041360c8ddbdec5359a
Sara Zayed <smmzayed@gmail.com>
2016-04-10 00:31:02 -0400

    Added tags hashtable to user class

--- PhotoAlbum22/src/app/User.java
+++ PhotoAlbum22/src/app/User.java
@@ -2,12 +2,22 @@ package app;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Hashtable;
 
 public class User implements Serializable {
 	//holds all information associated with a user
 	String name = "";
 	public ArrayList<Album> albums = new ArrayList<Album>();
+	public Hashtable<String,Photo> tags = new Hashtable<String,Photo>();
 	
+	public Hashtable<String, Photo> getTags() {
+		return tags;
+	}
+
+	public void setTags(Hashtable<String, Photo> tags) {
+		this.tags = tags;
+	}
+
 	public String getName() {
 		return name;
 	}
