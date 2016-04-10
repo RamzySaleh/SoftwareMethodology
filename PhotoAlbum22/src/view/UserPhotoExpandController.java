894949f61915cc37095ceb1acacf0dc69b98dc96
Ramzy Saleh <rs1064@scarletmail.rutgers.edu>
2016-04-10 16:36:38 -0400

    Clean-up, Tags added, Date Added

--- PhotoAlbum22/src/view/UserPhotoExpandController.java
+++ PhotoAlbum22/src/view/UserPhotoExpandController.java
@@ -1,56 +1,33 @@
 package view;
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Optional;
-
-import com.sun.javafx.iio.ImageStorage;
-
+import java.util.Calendar;
 import app.Album;
-import app.User;
 import view.MainController;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.geometry.Insets;
-import javafx.scene.control.Button;
-import javafx.scene.control.ButtonType;
-import javafx.scene.control.Dialog;
-import javafx.scene.control.Label;
-import javafx.scene.control.ListView;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.control.SplitPane;
-import javafx.scene.control.TextField;
-import javafx.scene.control.TitledPane;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.input.MouseButton;
-import javafx.scene.input.MouseEvent;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
-import javafx.scene.Node;
 import javafx.scene.Scene;
-import javafx.scene.control.Alert;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.TilePane;
-import javafx.scene.text.TextAlignment;
-import javafx.scene.control.Alert.AlertType;
 
 public class UserPhotoExpandController {
 	
 	@FXML BorderPane borderPane;
 	@FXML Button back;
 	@FXML Label timeOfCapture;
-	@FXML Label tags;
+	@FXML Label username;
+	@FXML ListView<String> tagListView;
+	@FXML Button logOut;
 	Album currentAlbum;
 	
 	MainController main = new MainController();
-	private ObservableList<String> obsList;
 	
 	public void init(MainController mainControl){
 		main = mainControl;
@@ -81,7 +58,11 @@ public class UserPhotoExpandController {
 			r.printStackTrace();
 		}
 	}
-	public void startPhotoExpand(ImageView image, Album album){
+	public void startPhotoExpand(ImageView image, Album album, int index){
+		
+		ObservableList<String> obsList = FXCollections.observableArrayList();
+		
+		username.setText(LoginController.currentUser.getName());
 		currentAlbum = album;
 		image.setFitWidth(225);
 		image.setFitHeight(225);
@@ -89,6 +70,17 @@ public class UserPhotoExpandController {
 		image.setCache(true);
 		image.setDisable(true);
 		borderPane.setCenter(image);
+		
+		String[] tags = album.getPhotos().get(index).tagsAsString();
+		
+		for (int i = 0; i < tags.length; i++) obsList.add(tags[i]);
+		
+		tagListView.setItems(obsList);
+		
+		Calendar cal = album.getPhotos().get(index).getTimeOfCapture();
+		
+		timeOfCapture.setText(String.valueOf(cal.getTimeInMillis()));
+		
 		
 	}
 
