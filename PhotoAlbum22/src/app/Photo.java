9e68deade5f7dbf44de6a6fc5d55267a5ac57afd
Ramzy Saleh <rs1064@scarletmail.rutgers.edu>
2016-04-10 16:38:58 -0400

    Hashtable for tags, date setting with file pointer

--- PhotoAlbum22/src/app/Photo.java
+++ PhotoAlbum22/src/app/Photo.java
@@ -1,17 +1,34 @@
 package app;
+import java.io.File;
 import java.io.Serializable;
-import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Hashtable;
 
-import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
 public class Photo implements Serializable {
 	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	ImageView image;
 	String caption;
+	public Hashtable<String, String> tagsHashTable = new Hashtable<String, String>();
 	
-
+	public String[] tagsAsString(){
+		String[] tagsAsSingleString = new String[tagsHashTable.size()];
+		
+		tagsAsSingleString = (String[]) tagsHashTable.values().toArray();
+		
+		return tagsAsSingleString;
+	}
+	
+	public void addTag(String key, String value){
+		this.tagsHashTable.put(key, value);
+	}
+	
 	public ImageView getImage() {
 		return image;
 	}
@@ -27,8 +44,11 @@ public class Photo implements Serializable {
 	public Calendar getTimeOfCapture() {
 		return timeOfCapture;
 	}
-	public void setTimeOfCapture(Calendar timeOfCapture) {
-		this.timeOfCapture = timeOfCapture;
+	public void setTimeOfCapture(File fp) {
+		SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss");
+		sdf.format(fp.lastModified());
+		timeOfCapture = sdf.getCalendar();
+		timeOfCapture.set(Calendar.MILLISECOND,0);
 	}
 	Calendar timeOfCapture;
 
